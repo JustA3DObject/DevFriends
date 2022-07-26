2ee2fff2341b62b5e80cb2b1da2520f9246cfdd6
Aayush <just.another.3d.object@gmail.com>
2022-07-26 21:31:11 +0530

    Made some test views and linked them

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+
+def room(request):
+    return HttpResponse('Room')
