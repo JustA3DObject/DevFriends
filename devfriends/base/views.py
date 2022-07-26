e5356a01eb9015c11f4de21259ae699b9355e3b3
Aayush <just.another.3d.object@gmail.com>
2022-07-26 22:07:54 +0530

    Restructuring

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -11,8 +11,8 @@ rooms = [
 
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
