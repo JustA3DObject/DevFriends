a2fad280a32f677c4606ecf5d527990b5f34efba
Aayush <just.another.3d.object@gmail.com>
2022-07-26 21:36:56 +0530

    Added templates directory and rendered templates

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
 
 
 def room(request):
-    return HttpResponse('Room')
+    return render(request, 'room.html')
--- devfriends/devfriends/settings.py
+++ devfriends/devfriends/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMP_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'devfriends.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMP_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
