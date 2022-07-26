b18053ab88dda2cd60245c48a10683a1b008b2fe
Aayush <just.another.3d.object@gmail.com>
2022-07-26 23:49:47 +0530

    Created model for Messages

--- devfriends/base/models.py
+++ devfriends/base/models.py
@@ -14,3 +14,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    # user =
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
