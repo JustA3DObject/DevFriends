868dcc1c65fb4a71d018a613780c2f20a048057c
Aayush <just.another.3d.object@gmail.com>
2022-07-26 23:54:35 +0530

    Added model for topics

--- devfriends/base/models.py
+++ devfriends/base/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
     # host =
-    # topic =
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     # participants =
