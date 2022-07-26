6f2bbca214cf69484c39e48a2c428548e6743ffb
Aayush <just.another.3d.object@gmail.com>
2022-07-26 23:44:51 +0530

    Connected Room model with views

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Python'},
-    {'id': 2, 'name': 'HTML'},
-    {'id': 3, 'name': 'JavaScript'},
-
-]
-
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
