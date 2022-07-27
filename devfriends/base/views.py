499b72fb45e3efb9df68c457943ed3d10ae1a484
Aayush <just.another.3d.object@gmail.com>
2022-07-28 00:18:59 +0530

    Added function to search by name & description

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from.forms import RoomForm
 
@@ -7,7 +8,9 @@ from.forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(description__icontains=q))
 
     topic = Topic.objects.all()
 
