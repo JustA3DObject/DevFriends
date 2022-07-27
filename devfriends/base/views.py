5f4f591bb987452c22fdd982ac6debd6860bd2b7
Aayush <just.another.3d.object@gmail.com>
2022-07-28 00:06:18 +0530

    Added condition if 'q' in None, show all rooms

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -6,7 +6,8 @@ from.forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name=q)
 
     topic = Topic.objects.all()
 
