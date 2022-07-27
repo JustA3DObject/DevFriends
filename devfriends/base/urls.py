dcf9a09a43b1057e209f9e20fafa87e35614e62c
Aayush <just.another.3d.object@gmail.com>
2022-07-28 01:59:36 +0530

    Linked logout user view

--- devfriends/base/urls.py
+++ devfriends/base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
