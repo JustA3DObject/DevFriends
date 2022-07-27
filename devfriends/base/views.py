152e7b01ace9b1e383cb08f09a585932e2273f67
Aayush <just.another.3d.object@gmail.com>
2022-07-28 02:46:54 +0530

    Added user creation form

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -18,7 +18,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -45,8 +45,18 @@ def logoutUser(request):
 
 
 def registerUser(request):
-    page = 'register'
-    return render(request, 'base/login_register.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
