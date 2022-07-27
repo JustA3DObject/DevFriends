e2283fab154753ed41170211c066f92fb7eafafb
Aayush <just.another.3d.object@gmail.com>
2022-07-28 00:46:45 +0530

    Created and linked login view

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -6,6 +6,11 @@ from.forms import RoomForm
 # Create your views here.
 
 
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) |
