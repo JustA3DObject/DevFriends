7b0dae6187f80fd215fc27f351a27ecfc83179de
Aayush <just.another.3d.object@gmail.com>
2022-07-28 16:06:38 +0530

    Added messages to room

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -77,7 +77,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all()
+    context = {'room': room, 'room_messages': room_messages}
 
     return render(request, 'base/room.html', context)
 
