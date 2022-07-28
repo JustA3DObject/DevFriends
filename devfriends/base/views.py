4845161a8f26d156f8abf0de775158c5ebd096f0
Aayush <just.another.3d.object@gmail.com>
2022-07-28 16:22:56 +0530

    Saving messages

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.forms import UserCreationForm
 
@@ -78,6 +78,13 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        messages = Message.objects.create(user=request.user,
+                                          room=room,
+                                          body=request.POST.get('body'))
+        return redirect('room', pk=room.id)
+
     context = {'room': room, 'room_messages': room_messages}
 
     return render(request, 'base/room.html', context)
