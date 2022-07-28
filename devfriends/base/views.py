20439aae4ae329f535c208f1408d1f34ebff6f21
Aayush <just.another.3d.object@gmail.com>
2022-07-28 17:21:28 +0530

    Applied ordering to entire Message model

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -79,7 +79,7 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         messages = Message.objects.create(user=request.user,
