8b45a8e8fdcb4df40bd9ad11599e5bc014647465
Aayush <just.another.3d.object@gmail.com>
2022-07-29 05:42:09 +0530

    Remade the form input in createRoom view

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -110,12 +110,15 @@ def createRoom(request):
     form = RoomForm()
     topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(host=request.user,
+                            topic=topic,
+                            name=request.POST.get('name'),
+                            description=request.POST.get('description'))
+
+        return redirect('home')
 
     context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
