8960cd54573cccad41e31ca3e413a7b52d702c75
Aayush <just.another.3d.object@gmail.com>
2022-07-29 05:55:29 +0530

    Edited update room view

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -134,10 +134,14 @@ def updateRoom(request, pk):
         return HttpResponse('You are not allowed here! ヽ(ಠ_ಠ) ノ ')
 
     if request.method == "POST":
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
         form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+
+        return redirect('home')
 
     context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
