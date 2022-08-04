e81378c22752a5ee8d9cc7901f0303e1d1db5625
Aayush <just.another.3d.object@gmail.com>
2022-08-05 04:24:41 +0530

    Edited login form

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -16,16 +16,16 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(
                 request, '¯\(°_o)/¯ Username or Password does not exist!')
         else:
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('home')
