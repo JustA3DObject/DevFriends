32efcbd99c1465778b720188b7eccdd5176bdb02
Aayush <just.another.3d.object@gmail.com>
2022-08-05 03:12:14 +0530

    Made migrations and debugging

--- devfriends/devfriends/settings.py
+++ devfriends/devfriends/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-MEDIA_ROOT = [os.path.join(BASE_DIR, "static/images")]
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
