c27405d6e2b70a41bcb3f2da806d80832f014b00
Aayush <just.another.3d.object@gmail.com>
2022-08-05 03:59:12 +0530

    Added custom fields to User form

--- devfriends/base/forms.py
+++ devfriends/base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room, User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class RoomForm(ModelForm):
@@ -12,4 +13,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['name', 'username', 'email', 'avatar', 'bio']
