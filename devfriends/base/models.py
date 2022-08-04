7b7e88037277f93d8e9e4bd44dbd18b107551eaf
Aayush <just.another.3d.object@gmail.com>
2022-08-05 03:07:34 +0530

    Linked MEDIA_URL % MEDIA_ROOT

--- devfriends/base/models.py
+++ devfriends/base/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=100, null=True)
     email = models.EmailField(unique=True)
     bio = models.TextField(null=True)
-    # avatar =
+    avatar = models.ImageField(null=True, default="avatar.svg")
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
