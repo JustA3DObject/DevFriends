c44a32219f13b5cd456f706e764922e9b437d1fc
Aayush <just.another.3d.object@gmail.com>
2022-08-05 02:56:29 +0530

    Imported custom user model in views.py & forms.py

--- devfriends/base/views.py
+++ devfriends/base/views.py
@@ -2,10 +2,9 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 
